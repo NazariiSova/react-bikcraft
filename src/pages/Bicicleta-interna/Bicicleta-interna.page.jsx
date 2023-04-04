8944f2b213952a21b7a27f74585f0f06823152e0
Nazarii Sova <madrigalamar@gmail.com>
2023-04-04 18:39:19 +0300

    create page bicicleta-interna before fixing

--- src/pages/Bicicleta-interna/Bicicleta-interna.page.jsx
+++ src/pages/Bicicleta-interna/Bicicleta-interna.page.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Footer from "../../components/footer/Footer";
+import { Link } from "react-router-dom";
 
 import { BicicletaInterna } from "../../components/Сhoice-of-bikes/ChoiceBikes";
 import { OtherBikes } from "../../components/Others-of-bikes/OthersBikes";
 
-import "./Bicicleta-internaPage.scss"
+import "./Bicicleta-internaPage.scss";
+
+import seguros from "./imgBicicletas/seguros.png";
+import dec from "../../components/img/dec.svg"
 
 // const dataBikeCatalog = [
 //   {
@@ -44,6 +48,23 @@ const BicicletaInternaPage = () => {
         <div className="bicicleta-choice-title">ESCOLHA A SUA</div>
         <OtherBikes />
       </div>
+      <div className="seguros-container">
+        <div className="seguros-img-box">
+          <img className="seguros-img" src={seguros} alt="seguros" />
+        </div>
+        <div className="seguros-content-box">
+          <div className="seguros-content-title">
+            pedale mais tranquilo com nosso 
+          </div>
+          <div className="seguros-content-description">
+            Inscreva-se em um dos planos do nosso seguro Bikecraft e aproveite
+            diversos benefícios.
+          </div>
+          <Link className="seguros-content-btn" to="/termos" >conheça mais</Link>
+        </div>
+        <div className="seguros-dec-box"><img className="seguros-dec" src={dec}/><img className="seguros-dec" src={dec}/></div>
+      </div>
+      <div className="seguros-blak-space"></div>
       <Footer />
     </div>
   );
