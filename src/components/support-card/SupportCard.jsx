42c720f605813d1c338a864b45cba49f770ed412
Nazarii Sova <madrigalamar@gmail.com>
2023-04-24 17:47:01 +0300

    start work with seguroPage

--- src/components/support-card/SupportCard.jsx
+++ src/components/support-card/SupportCard.jsx
@@ -1,16 +1,15 @@
 import { Link } from "react-router-dom";
 import "./support-card.scss";
 export const SupportCard = ({ isGold, title, price, list }) => {
-    let type = isGold ? 'gold' : 'silver';
+  let type = isGold ? "gold" : "silver";
   return (
     <div className={`support-${type}-container`}>
-       <div className={`support-${type}-left`}>
-       <div className={`support-${type}-title`}>{title}</div>
+      <div className={`support-${type}-left`}>
+        <div className={`support-${type}-title`}>{title}</div>
         <div className={`support-${type}-list`}>
           <ul className={`support-${type}-ul`}>
-
-            {list.map((item)=>(
-                <li key={item}>{item}</li>
+            {list.map((item) => (
+              <li key={item}>{item}</li>
             ))}
           </ul>
         </div>
@@ -25,6 +24,5 @@ export const SupportCard = ({ isGold, title, price, list }) => {
         </Link>
       </div>
     </div>
-    
   );
 };
--- src/pages/Seguros/Seguros.page.jsx
+++ src/pages/Seguros/Seguros.page.jsx
@@ -1,22 +1,82 @@
 import React from "react";
+import "./segurosPage.scss";
 import Footer from "../../components/footer/Footer";
+import { SupportCard } from "../../components/support-card/SupportCard";
+
 const SegurosPage = () => {
-    return (
-        <div className="bicicletas-page-container">
-        <div className="header-container">
-            <div className="header-small-text">ESCOLHA O SEGURO </div>
-            <div className="header-title">você seguro</div>
+  return (
+    <div className="bicicletas-page-container">
+      <div className="header-container">
+        <div className="header-small-text">ESCOLHA O SEGURO </div>
+        <div className="header-title">você seguro</div>
+      </div>
+      <div className="seguro-container">
+        <div className="seguro-support-card">
+          <SupportCard
+            title={"PRATA"}
+            list={[
+              "Duas trocas por ano",
+              "Assistência técnica",
+              "Suporte 08h às 18h",
+              "Cobertura estatual",
+            ]}
+            price={"R$ 199"}
+            isGold={false}
+          />
+          <SupportCard
+            title={"OURO"}
+            list={[
+              "Cinco trocas por ano",
+              "Assistência especial",
+              "Suporte 24h",
+              "Cobertura nacional",
+              "Descontos em parceiros",
+              "Acesso ao Clube Bikecraft",
+            ]}
+            price={"R$ 299"}
+            isGold={true}
+          />
         </div>
-        <div className="bicicletas-catalog-container">
-
-
-            
+        <div className="seguro-advantages-container">
+          <div className="seguro-advantages-title">nossas vantagens</div>
+          <div className="seguro-advantages-items-container">
+            <div className="seguro-advantages-item-box">
+                <img className="seguro-advantages-item-img" src="" alt="bike" />
+                <div className="seguro-advantages-item-title"></div>
+                <div className="seguro-advantages-item-description"></div>
+            </div>
+            <div className="seguro-advantages-item-box">
+                <img className="seguro-advantages-item-img" src="" alt="material" />
+                <div className="seguro-advantages-item-title"></div>
+                <div className="seguro-advantages-item-description"></div>
+            </div>
+            <div className="seguro-advantages-item-box">
+                <img className="seguro-advantages-item-img" src="" alt="support" />
+                <div className="seguro-advantages-item-title"></div>
+                <div className="seguro-advantages-item-description"></div>
+            </div>
+            <div className="seguro-advantages-item-box">
+                <img className="seguro-advantages-item-img" src="" alt="tracker" />
+                <div className="seguro-advantages-item-title"></div>
+                <div className="seguro-advantages-item-description"></div>
+            </div>
+            <div className="seguro-advantages-item-box">
+                <img className="seguro-advantages-item-img" src="" alt="Safety" />
+                <div className="seguro-advantages-item-title"></div>
+                <div className="seguro-advantages-item-description"></div>
+            </div>
+            <div className="seguro-advantages-item-box">
+                <img className="seguro-advantages-item-img" src="" alt="speed" />
+                <div className="seguro-advantages-item-title"></div>
+                <div className="seguro-advantages-item-description"></div>
+            </div>
+          </div>
         </div>
-        
+      </div>
 
-        <Footer/>
+      <Footer />
     </div>
-    )
+  );
 };
 
 export default SegurosPage;
