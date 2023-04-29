20d90ac81293fe06a97e9e5b2821e9044c007a3c
Nazarii Sova <madrigalamar@gmail.com>
2023-04-29 18:45:05 +0300

    start work with contatoPage

--- src/pages/Contato/Contato.page.jsx
+++ src/pages/Contato/Contato.page.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import "./contatoPage.scss"
+
+import facebook from "../../components/img/Icons/facebook.png"
+import instagram from "../../components/img/Icons/instagram.png"
+import map from "../../components/img/Icons/map.png"
+import message from "../../components/img/Icons/message.png"
+import phone from "../../components/img/Icons/phone.png"
+import toutube from "../../components/img/Icons/toutube.png"
+
 const ContatoPage = () => {
     return (
         <div className="contacts-container">
@@ -7,7 +15,19 @@ const ContatoPage = () => {
                 <div className="contacts-header-title-small">RESPOSTAS EM ATÉ 24H</div>
                 <div className="contacts-header-title">nosso contato</div>
             </div>
-            <div className="contacts-"></div>
+            <div className="contacts-shops-login-container">
+                <div className="contacts-shops">
+                    <div className="contacts-shops-title">Loja Online</div>
+                    <div className="contacts-shops-location">
+                        <img className="contacts-shops-img"></img>
+                        <ul className="contacts-shops-location-list">
+
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="contacts-login"></div>
+            </div>
         </div>
     )
 };
