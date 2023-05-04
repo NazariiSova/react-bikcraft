32461fba627a98164b0c86c4d0cf80fc1d6083a7
Nazarii Sova <madrigalamar@gmail.com>
2023-05-04 15:11:36 +0300

    add termos page with list

---
+++ src/components/termosList/termosList.jsx
@@ -0,0 +1,28 @@
+const data = [
+    {
+      title: "1. Política de Estornos",
+      list: [
+        "1.1 Cartões de crédito: Visa, MasterCard, ELO e American Express. Para pagamentos à vista aceitamos PIX e Boleto através do PageSeguro.",
+        "1.2 Cartões de Crédito: Visa, MasterCard, ELO e American Express. Para pagamentos à vista aceitamos PIX e Boleto através do PageSeguro.",
+        "1.3 Cartões de Crédito: Visa, MasterCard, ELO e American Express. Para pagamentos à vista aceitamos PIX e Boleto através do PageSeguro."
+      ]
+    },
+    {
+      title: "2. Conta do Usuário",
+      list: [
+        "2.1 Cartões de crédito aceites: Visa, MasterCard, ELO e American Express. Para pagamentos à vista aceitamos PIX e Boleto através do PageSeguro.",
+        "2.2 Cartões de Crédito: Visa, MasterCard, ELO e American Express. Para pagamentos à vista aceitamos PIX e Boleto através do PageSeguro.",
+        "2.3 Cartões de Crédito: Visa, MasterCard, ELO e American Express. Para pagamentos à vista aceitamos PIX e Boleto através do PageSeguro."
+      ]
+    },
+    {
+      title: "3. Seguro BikCraft",
+      list: [
+        "3.1 Cartões de crédito adicionais: Visa, MasterCard, ELO e American Express. Para pagamentos à vista aceitamos PIX e Boleto através do PageSeguro.",
+        "3.2 Cartões de Crédito: Visa, MasterCard, ELO e American Express. Para pagamentos à vista aceitamos PIX e Boleto através do PageSeguro.",
+        "3.3 Cartões de crédito adicionais: Visa, MasterCard, ELO e American Express. Para pagamentos à vista aceitamos PIX e Boleto através do PageSeguro."
+      ]
+    }
+  ];
+
+  export default data
--- src/pages/Termos/Termos.page.jsx
+++ src/pages/Termos/Termos.page.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import "./termosPage.scss";
+import Footer from "../../components/footer/Footer";
+import data from "../../components/termosList/termosList"
+
 const TermosPage = () => {
-    return (
-        <div>
-            fudghfy dfuygfuyd udsghfugdui
-        </div>
-    )
+  return (
+    <div className="termos-container">
+      <div className="termos-header">
+        <div className="termos-header-title-small">TERMOS DE USO</div>
+        <div className="termos-header-title">termos e condições</div>
+      </div>
+      <div className="termos-list-container">
+           <div className="termos-list-box">
+        {data.map((item) => (
+          <li className="termos-list-title-box" key={item.title}>
+            <h3 className="termos-list-title">{item.title}</h3>
+            <ul className="termos-list-description-box">
+              {item.list.map((listItem, index) => (
+                <li className="termos-list-description" key={index}>{listItem}</li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default TermosPage;
